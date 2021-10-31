0aa9dd698241a860efa521f414d5f634d2767556
ArerepadeB <aredelulu@gmail.com>
2021-10-31 11:57:20 -0500

    tfc cicd

--- dev/config.tf
+++ dev/config.tf
@@ -19,3 +19,15 @@ terraform {
     }
   }
 }
+
+terraform {
+  required_version = ">=0.12"
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "acm_provider"
+  region = "us-east-1"
+}
