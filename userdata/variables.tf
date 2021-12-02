7e66ef81d0244130dee2351f272e8930ede71e12
ArerepadeB <aredelulu@gmail.com>
2021-12-02 13:24:55 -0600

    update

--- userdata/variables.tf
+++ userdata/variables.tf
@@ -2,3 +2,8 @@ variable "instance_type" {
   type        = string
   description = "instance size for ec2"
 }
+
+variable "region" {
+    type = string
+    description = "region to deploy resources"
+}
