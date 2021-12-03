c891920078e787e739494f828504dde2e6cdac6b
ArerepadeB <aredelulu@gmail.com>
2021-12-02 23:48:03 -0600

    fixing port

--- userdata/sg.tf
+++ userdata/sg.tf
@@ -33,12 +33,12 @@ resource "aws_security_group" "ec2-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = [aws_security_group.main-alb.id]
-  }
+  # ingress {
+  #   from_port   = 8080
+  #   to_port     = 8080
+  #   protocol    = "tcp"
+  #   cidr_blocks = [aws_security_group.main-alb.id]
+  # }
 
   ingress {
     from_port       = 443
