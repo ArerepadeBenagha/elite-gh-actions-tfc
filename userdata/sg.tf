dda2812320747c87d3f8b315800ffbc40d210fb3
ArerepadeB <aredelulu@gmail.com>
2021-12-02 23:09:39 -0600

    adding alb

--- userdata/sg.tf
+++ userdata/sg.tf
@@ -33,6 +33,12 @@ resource "aws_security_group" "ec2-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [aws_security_group.main-alb.id]
+  }
+  ingress {
     from_port       = 443
     to_port         = 443
     protocol        = "tcp"
@@ -58,6 +64,12 @@ resource "aws_security_group" "main-alb" {
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
