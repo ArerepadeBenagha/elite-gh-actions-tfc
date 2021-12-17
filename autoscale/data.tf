ec10c71cd54ad9df2a02fe62d5d86805c7ac382b
ArerepadeB <aredelulu@gmail.com>
2021-12-17 17:49:17 -0600

    autoscale

--- autoscale/data.tf
+++ autoscale/data.tf
@@ -1,25 +1,25 @@
-data "cloudinit_config" "userdata" {
-  gzip          = true
-  base64_encode = true
+# data "cloudinit_config" "userdata" {
+#   gzip          = true
+#   base64_encode = true
 
-  part {
-    content_type = "text/x-shellscript"
-    filename     = "userdata_jenkins"
-    content      = templatefile("../templates/userdata_jenkins.tpl", {})
-  }
-}
-data "aws_ami" "ubuntu" {
-  most_recent = true
+#   part {
+#     content_type = "text/x-shellscript"
+#     filename     = "userdata_jenkins"
+#     content      = templatefile("../templates/userdata_jenkins.tpl", {})
+#   }
+# }
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  owners = ["099720109477"] # Canonical
-}
+#   owners = ["099720109477"] # Canonical
+# }
